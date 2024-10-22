0423829e20adfb49006cff8a2fca072d4d9535df
Dinara <dinara.kspanovr@gmail.com>
2024-10-22 11:12:12 +0600

    first commit 2

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ buttons.forEach((btn) => {
 
 // git init
 // 
-//  README.md
+// git add  .  
 // git commit -m  "first commit"
 // git branch -M main
 // git remote add origin https://github.com/dinara6666/lesson6b.git
